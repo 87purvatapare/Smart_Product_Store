1b8d46795cc70ea040407dd782a7ed8fd43b6ed8
87purvatapare <purvatapare@gmail.com>
2025-07-31 23:53:54 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -29,13 +29,7 @@ app.use(cors({
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
-// ✅ Serve frontend in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-//   );
-// }
+
 
 app.listen(PORT, () => {
   connectDB();
