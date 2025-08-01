7a960146de1c9271e445ccd24545202e9d47ce97
87purvatapare <purvatapare@gmail.com>
2025-08-01 15:34:35 +0530

    updated files

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -4,8 +4,8 @@ import Product from "../models/product.model.js";
 export const getProducts = async (req, res) => {
 	try {
 		const products = await Product.find({});
-		// res.status(200).json({ success: true, data: products });
-		res.status(200).json(products);
+		res.status(200).json({ success: true, data: products });
+		//res.status(200).json(products);
 	} catch (error) {
 		console.log("error in fetching products:", error.message);
 		res.status(500).json({ success: false, message: "Server Error" });
