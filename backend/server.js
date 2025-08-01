ad2f5d2d983725460a91f66d2d8c24eb11398d71
87purvatapare <purvatapare@gmail.com>
2025-08-01 14:19:33 +0530

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://smart-product-store-1.onrender.com"
+    "https://smart-product-store-2.onrender.com"
   ],
   credentials: true
 }));
